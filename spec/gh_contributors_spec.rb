c0dd32c87b71ea9290436ae81c6d86aa26c354c9
Jordon Bedwell <envygeeks@gmail.com>
2013-02-14 00:22:39 -0600

    Consistency.

--- spec/gh_contributors_spec.rb
+++ spec/gh_contributors_spec.rb
@@ -3,21 +3,20 @@ require 'shared_examples_for_for_org'
 require 'shared_examples_for_for_repo'
 
 describe GhContributors do
-
-  describe ".for_org" do
-    it_behaves_like 'for_org', GhContributors
+  describe "#for_org" do
+    it_behaves_like "for_org", GhContributors.new
   end
 
-  describe "#for_org" do
-    it_behaves_like 'for_org', GhContributors.new
+  describe ".for_org" do
+    it_behaves_like "for_org", GhContributors
   end
 
   describe ".for_repo" do
-    it_behaves_like 'for_repo', GhContributors
+    it_behaves_like "for_repo", GhContributors
   end
 
   describe "#for_repo" do
-    it_behaves_like 'for_repo', GhContributors.new
+    it_behaves_like "for_repo", GhContributors.new
   end
 
 end
