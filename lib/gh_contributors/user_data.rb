d28dbe4b4594625cab6d15e3a829568cc4f4971a
Michal Papis <mpapis@gmail.com>
2013-02-14 10:01:36 +0100

    allow adding extra plugins from custom/user classes

--- lib/gh_contributors/user_data.rb
+++ lib/gh_contributors/user_data.rb
@@ -10,27 +10,30 @@ class GhContributors
         data.map{|repo| repo['contributions'].to_i}.inject(&:+)
       end
 
-      def self.type
+      def self.handles
         self.name.split(/::/).last.downcase.to_sym
       end
+    end
 
-      def self.handles?(type)
-        type == self.type
-      end
+    def self.find(type)
+      @rvm2plugins ||= Pluginator.new("gh_contributors")
+      @rvm2plugins["user_data"].detect{ |_,plugin| plugin.handles == type }
     end
 
     def self.load(type, login, data)
       validate!(type)
-      @rvm2plugins["user_data"].detect{ |_,plugin| plugin.handles?(type) }.last.load(login, data)
+      find(type).last.load(login, data)
     end
 
     def self.validate!(type)
-      @rvm2plugins ||= Pluginator.new("gh_contributors")
-      unless @rvm2plugins["user_data"].map{|_,klass| klass.type }.include?(type)
+      if find(type).nil?
         raise UnknownUserDetail.new("Unknown user_details: #{type}")
       end
     end
+
+    def self.register_class(klass)
+      @rvm2plugins ||= Pluginator.new("gh_contributors")
+      @rvm2plugins.register_plugin_class("user_data", klass)
+    end
   end
 end
-
-
